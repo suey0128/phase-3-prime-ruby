f255baa9375dad7977518e5105dbccd72d308df3
suey0128 <syui0128@gmail.com>
2021-06-27 23:14:45 -0700

    commit tree c14dc517d9881537f35e350e62f6ea6cfd97a009

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime? num
+    if num <= 1
+        return false 
+    elsif num === 2 || num === 3
+        return true
+    else
+        # (2..(int-1)).each{|i| return false if int % i == 0}
+        # return true
+        n = 2
+        while n < num
+            if num%n === 0
+            return false  
+            end
+            n += 1
+        end
+    end 
+    return true
+end
